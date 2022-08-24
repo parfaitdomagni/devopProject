be70fbc346e1bdbf2cd6e033b3c9f80d457caf60
parfait <bparfa@gmail.com>
2022-08-24 18:33:54 -0500

    this the first commit for the file

---
+++ project1.java
@@ -0,0 +1,4 @@
+
+
+
+n this guide, we’ll show how to check the status of NGINX on Ubuntu. This will give you information about the state of the NGINX service, to help you determine if it’s running, accepting connections successfully, etc. We’ll also explain the various states of NGINX, so you know what to do with the information that’s presented.
